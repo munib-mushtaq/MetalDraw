4f1609dc9374af481945590abbf47c64bb3bf61d
Arslan Raza <arslan.raza@invozone.com>
2024-01-15 17:46:08 +0500

    Initial Commit

---
+++ MetalFramework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetalFramework
+//
+//  Created by Arslan Raza on 15/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
